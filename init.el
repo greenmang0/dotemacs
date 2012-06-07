c7bf15dca876a042fa731cedf614d12b0d0d158b
Abhishek Amberkar <abhishek.amberkar@gmail.com>
2012-06-07 13:42:12 +0530

    Change scratch message

--- init.el
+++ init.el
@@ -91,26 +91,8 @@
 (defun live-welcome-message ()
   (nth (random (length live-welcome-messages)) live-welcome-messages))
 
-(setq initial-scratch-message (concat ";;     MM\"\"\"\"\"\"\"\"`M
-;;     MM  mmmmmmmM
-;;     M`      MMMM 88d8b.d8b. .d8888b. .d8888b. .d8888b.
-;;     MM  MMMMMMMM 88''88'`88 88'  `88 88'  `\"\" Y8ooooo.
-;;     MM  MMMMMMMM 88  88  88 88.  .88 88.  ...       88
-;;     MM        .M dP  dP  dP `88888P8 '88888P' '88888P'
-;;     MMMMMMMMMMMM
-;;
-;;         M\"\"MMMMMMMM M\"\"M M\"\"MMMMM\"\"M MM\"\"\"\"\"\"\"\"`M
-;;         M  MMMMMMMM M  M M  MMMMM  M MM  mmmmmmmM
-;;         M  MMMMMMMM M  M M  MMMMP  M M`      MMMM
-;;         M  MMMMMMMM M  M M  MMMM' .M MM  MMMMMMMM
-;;         M  MMMMMMMM M  M M  MMP' .MM MM  MMMMMMMM
-;;         M         M M  M M     .dMMM MM        .M
-;;         MMMMMMMMMMM MMMM MMMMMMMMMMM MMMMMMMMMMMM
-;;
-;;           http://github.com/greenmang0/dotemacs
-;;
+(setq initial-scratch-message (concat "
 ;; " (live-welcome-message) "
-
 "))
 
 ;;;
@@ -118,7 +100,7 @@
 ;;;
 
 ;;default live packs
-(setq live-packs '("core" "color" "power" "clojure" "programming" "notes"))
+(setq live-packs '("core" "themes" "power" "clojure" "programming" "notes"))
 
 ;; Load all packs - Power Extreme!
 (live-load-all-packs live-packs)
